b8daddfe44a6813e7d1bbfb39fca8238b45960dc
ForkKILLET <fork_killet@qq.com>
2020-10-02 07:47:13 +0000

    VER 1.1 Config
    -> $ xbqg %             # List whole <$XBQG_DATA/setting.json>.
    -> $ xbqg % .key        # If JSON is `{ "key": "value" }`, outputs `"value"`.
    -> $ xbqg % .key.other  # If JSON is `{ "key": { "other": "value 2" } }`, outputs `"value 2"`.
    -> $ xbqg % key         # The begining "." is not necessary.
    -> $ xbqg % [0]         # If JSON is `[ "value 3" ]`, outputs `"value 3"`.
    -> $ xbqg % a.b[1].c    # If JSON is `{ "a": { "b": [ null, { "c": "OK" } ] } }`, outputs `"OK"`.
    -> $ xbqg % a           # If JSON is same as above, outputs `{ "b": [ null, { "c": "OK" } ] }`.
    -> $ xbqg % a \"b\"     # Set ".a" to "b"
    -> $ xbqg % a =b        # If you hate escapes, use this. Space in front of `b` is necessary.
    -> $ xbqg % a undefined # Delete ".a"
    -> $ xbqg % a x         # Delete ".a"
    -> $ xbqg config        # The full name of `xbqg %`

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 // :: Import
 
 const fs    = require("fs")
-const http  = require("https") // Note: xbqg deny http
+const http  = require("https") // Note: Xbqg refuses http requests.
 
 // :: Tool
 
@@ -14,6 +14,12 @@ function Err(m) {
     if (debug) throw m
     else console.error(m)
     process.exit()
+}
+
+function existFile(path) {
+    return new Promise(resolve =>
+        fs.exists(path, exist => resolve(exist))
+    )
 }
 
 function readJSON(path, noParse) {
@@ -33,7 +39,7 @@ function writeJSON(path, data) {
         fs.writeFile(path, new Uint8Array(
             Buffer.from(typeof data === "string"
                 ? data
-                : JSON.stringify(data)
+                : JSON.stringify(data, null, 4)
             )
         ), "utf8", err => {
             if (err) reject(err)
@@ -62,7 +68,6 @@ async function ajax(tar) {
             res.on("end", () => resolve(data))
         })
     })
- 
 }
 
 // :: Script
@@ -79,7 +84,7 @@ function div(t, u, d) {
         "\n".repeat(u ?? 1) +
         "-".repeat(10) +
         "=".repeat(5) + 
-        (t ? ` ${t} ` : "") +
+        (t ? ` \x1B[1;34m${t}\x1B[0m ` : "") +
         "=".repeat(5) +
         "-".repeat(10) +
         "\n".repeat(d ?? 1)
@@ -104,7 +109,7 @@ async function writeConfig(filename, data) {
 
 function fetchAlias(name) {
     return async() => {
-        div("page info", 0, 1)
+        div("page info", 0, 2)
         arround = await readConfig("arround")
         if (arround[name]) {
             console.log(`${name}: ${arround[name]}`)
@@ -112,19 +117,23 @@ function fetchAlias(name) {
         }
         else {
             console.log(`${name}: null`)
-            div("EOF", 0, 1)
+            div("EOF", 1, 1)
         }
     }
 }
 
 const verbs = {
     // Note: alias
-    "[": "prev",
-    "=": "curr",
-    "]": "next",
-    "-": "arro",
-    ":": "fetch",
-    "?": "help",
+    "[":    "prev",
+    "=":    "curr",
+    "]":    "next",
+    "-":    "arround",
+    "@+":   "bookmark",
+    "@-":   "bookcase",
+    "@:":   "bookfetch",
+    ":":    "fetch",
+    "?":    "help",
+    "%":    "config",
 
     // Note: work
 
@@ -137,25 +146,27 @@ const verbs = {
         let _title = _html.match(/<title>(.*)<\/title>/)[1]
         if (_title.match(/^[3-5][01]\d+/))
             Err(`fetch: HTTP error code: ${_title}`)
-        _title = _title.replace(/- 新笔趣阁.*/, "\n").replace("-", " @ ")
+        _title = _title.replace(/ - 新笔趣阁.*/, "").replace("-", " @ ")
         const _content = _html.match(/<div id="content">(.*)<\/div>/)[1]
             .replace(/<br[ ][\/]>/g, "\n").replace(/&nbsp;/g, " ")
         div("text", 1, 2)
-        console.log(_title, _content)
+        console.log(_title, "\n", _content)
 
         div("arround", 1, 2)
         const
             _arround = _html.match(/=keypage;([\s\S]*)function keypage/)[1],
-            _prev = _arround.match(/prevpage="\/(.*).html"/)[1],
-            _next = _arround.match(/nextpage="\/(.*).html"/)[1],
-            arroundJSON = `{
-    "prev": "${_prev}",
+            _prev = _arround.match(/prevpage="\/(.*).html"/)?.[1],
+            _next = _arround.match(/nextpage="\/(.*).html"/)?.[1],
+            arroundJSON =
+`{
+    "prev": ${_prev ? '"' + _prev + '"' : "null"},
     "curr": "${page}",
-    "next": "${_next}"
-}`
+    "next": ${_next ? '"' + _next + '"' : "null"},
+    "title": "${_title}"
+}\n`
         console.log(arroundJSON)
-        await writeConfig("arround", arroundJSON + "\n")
-        div("EOF", 1, 1)
+        await writeConfig("arround", arroundJSON)
+        div("EOF", 0, 1)
     },
 
     help: () => {
@@ -168,14 +179,105 @@ const verbs = {
     curr: fetchAlias("curr"),
     next: fetchAlias("next"),
 
-    arro: async () => {
+    arround: async() => {
         div("arround", 0, 2)
         console.log(await readConfig("arround", true))
         div("EOF", 0, 1)
+    },
+
+    bookcase: async() => {
+        div("bookcase", 0, 2)
+        // TODO: better display
+        console.log(await readConfig("books", true))
+        div("EOF", 1, 1) 
+    },
+
+    bookmark: async() => {
+        const arround = await readConfig("arround")
+        const key = arround.curr.match(/(.*)\//)[1]
+        const books = await readConfig("books")
+        
+        let newBook = ! books[key]
+        books[key] = arround
+        books[key].name = args[1]
+        await writeConfig("books", books)
+
+        div("bookmark", 0, 1)
+        console.log(newBook ? "Added." : "Updated.")
+        div("EOF", 0, 1)
+    },
+
+    bookfetch: async() => {
+        const name = args[1]
+        if (!name) Err("bookfetch: book name can't be null.")
+
+        div("bookfetch", 0, 2)
+        const books = await readConfig("books")
+        for (let i in books) if (name === i || name === books[i].name) {
+            console.log("Succeeded.")
+            await verbs.fetch(books[i].curr)
+            return
+        }
+        console.log("Not found.")
+        div("EOF", 1, 1)
+    },
+
+    config: async() => {
+        if (args[1] == null) {
+            div("config list global", 0, 2)
+            console.log(await readConfig("setting", true))
+            div("EOF", 1, 1)
+        }
+        else {
+            const W = !! args[2]
+
+            div(W ? "config write" : "config read", 0, 2)
+
+            let config = await readConfig("setting"),
+                path = args[1]
+            if (path[0] !== "." && path[0] !== "[") path = "." + path
+
+            const rsit = path.matchAll(/\.([_a-zA-Z][0-9_a-zA-Z]*)|\[(\d+)]/g)
+            let rs, c = config, pa, f = true, t, key
+            while (! (rs = rsit.next()).done) {
+                t = !! rs.value[1], key = t ? rs.value[1] : Number(rs.value[2])
+                if (c && typeof c === "object") {
+                    if ((t && ! Array.isArray(c))
+                    || (!t && Array.isArray(c))) {
+                        pa = c
+                        c = pa[key]
+                    }
+                    else {
+                        if (W) Err("c: Path type conflicted.")
+                        console.log(undefined)
+                        f = false; break
+                    }
+                }
+                else if (c === undefined) W && (c = pa[key] = (t ? {} : []))
+                else W && Err("c: Path type conflicted.")
+            }
+            if (W) {
+                pa[key] = [ "undefined", "x" ].includes(args[2])
+                    ? undefined
+                    : JSON.parse(args[2][0] === "=" ? '"' + args[2].slice(1) + '"' : args[2])
+                await writeConfig("setting", config)
+            }
+            if (f) console.log(JSON.stringify(W ? config : c, null, 4))
+            div("EOF", 1, 1)
+        }
     }
 }
 
 async function init() {
+    const configDft = {
+        setting:    "{}",
+        arround:    "{}",
+        books:      "{}"
+    }
+    for (let i in configDft)
+        if (! await existFile(`${p_data}/${i}.json`))
+            writeConfig(i, configDft[i])
+
     setting = await readConfig("setting")
 
     let done = false; while (!done) {
@@ -186,10 +288,7 @@ async function init() {
                 await verbs[verb]()
                 done = true; break
             default:
-                Err(`xbqg: unreachable. verb: ${verb}
-I think the world crashed, don't worry -
-Because the matter is big, and worrying is useless.`)
-                // wWw: 别慌，事情很大，慌也没用
+                Err(`xbqg: Unknown verb: ${verb}`)
         }
     }
 }
