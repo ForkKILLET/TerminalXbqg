a598412fc20fdf238975752f32d74b4e2f37b0df
ForkKILLET <fork_killet@qq.com>
2020-10-05 03:00:38 +0000

    VER 1.4 Better config: edit manually
    -> $ xbqg % editor = "vi \${path}"  # Config your editor first. Notice `${path}`.
    -> $ xbqg %+                        # Now vi will open <$XBQG_DATA/setting.json>.
    -> $ xbqg %+ arround                # if you want to edit other JSON files.
    -> $ xbqg {config_edit,ce}          # Aliases.
     : "Editor" needn't to be a real editor. Something like cat is OK.
     : I mean some guy would config your editor to `rm -f ${path}`. XD
    -> $ xbqg %=                        # Reseting to the default.
    -> $ xbqg {config_reset,cr}         # Aliases.
     : You can stop it by `^C` in 5 sec. Oh, 4 sec now.
    -- Each verb has 3 names now: symbol, full English and abbr English.
    -- The modified setting is highlighted now.
    -" TODO: Better pagewarner, multi-source, cache.
    -" What? Help? Forget it!

--- main.js
+++ main.js
@@ -3,24 +3,27 @@
 // :: Import
 
 const fs    = require("fs")
-const http  = require("https") // Note: Xbqg refuses http requests.
-require("fkutil") // TODO: refactor with it
+const execa = require("execa")
+const {
+    Is, sleep, ajax
+}           = require("fkutil") // TODO: refactor with it
 
 // :: Tool
 
 const debug = false
 
-function Warn(m) {
-    console.warn(`\x1B[33m${m}\x1B[0m`)
+function Warn(...m) {
+    console.warn(`\x1B[33m${m.join(" ")}\x1B[0m`)
 }
-function Err(m) {
-    m = `\x1B[31m${m}\x1B[0m`
+function Err(...m) {
+    m = `\x1B[31m${m.join(" ")}\x1B[0m`
     if (debug) throw m
-    else console.error(m)
+    console.error(m)
     div("EOF", 1, 1)
     process.exit()
 }
 function Log(...m) { console.log(...m) }
+function Hili(m) { return `\x1B[32m${m}\x1B[0m` }
 
 function existFile(path) {
     return new Promise(resolve =>
@@ -43,7 +46,7 @@ function readJSON(path, noParse) {
 function writeJSON(path, data) {
     return new Promise((resolve, reject) => {
         fs.writeFile(path, new Uint8Array(
-            Buffer.from(typeof data === "string"
+            Buffer.from(Is.str(data)
                 ? data
                 : JSON.stringify(data, null, 4)
             )
@@ -54,30 +57,9 @@ function writeJSON(path, data) {
     })
 }
 
-async function ajax(url, charset) {
-   return new Promise((resolve, reject) => {
-        http.get(url, res => {
-            const { statusCode } = res
-            const contentType = res.headers['content-type']
-            
-            if (statusCode !== 200) {
-                res.resume()
-                reject(new Error(`ajax: Request failed.\n` + 
-                    `Status Code: ${statusCode}\n` +
-                    `URL: ${url}`
-                ))
-            }
-
-            let data = ""
-            res.on("data", chunk => data += chunk)
-            res.on("end", () => resolve(data))
-        })
-    })
-}
-
 // :: Script
 
-const [ interpreter, script, ...args ] = process.argv
+const [ , , ...args ] = process.argv
 let verb = args?.[0] ?? "help"
 if (verb.match(/\d+_\d+\/\d+/)) {
     args[1] = verb
@@ -99,7 +81,7 @@ function div(t, u, d) {
 const p_data = process.env.XBQG_DATA.replace(/\/$/, "")
 if (!p_data) Err("xbqg: Please configure the environment variable `XBQG_DATA` to a non-root dir.")
 
-let arround, setting, books
+let arround, setting
 
 async function readConfig(filename, noParse) {
     const o = await readJSON(`${p_data}/${filename}.json`, noParse)
@@ -130,16 +112,25 @@ function fetchAlias(name) {
 const verbs = {
     // :::: alias
 
-    "[":    "prev",
-    "=":    "curr",
-    "]":    "next",
-    "-":    "arround",
-    "@+":   "bookmark",
-    "@-":   "bookcase",
-    "@:":   "bookfetch",
-    ":":    "fetch",
-    "?":    "help",
-    "%":    "config",
+    ":":    "fetch",            "f":    "fetch",
+
+    "[":    "fetch_prev",       "fp":   "fetch_prev",
+    "=":    "fetch_curr",       "fc":   "fetch_curr",
+    "]":    "fetch_next",       "fn":   "fetch_next",
+    "-":    "arround",          "a":    "arround",
+
+    "@-":   "book_list",        "bs":   "book_show",
+    "@+":   "book_mark",        "bm":   "book_mark",
+    "@:":   "book_fetch",       "bf":   "book_fetch",
+
+    "%":    "config",           "c":    "config",
+    "%+":   "config_edit",      "ce":   "config_edit",
+    "%=":   "config_reset",     "cr":   "config_reset",
+
+    "^-":   "pagewarner_stat",  "ps":   "pagewarner_stat",
+    "^=":   "pagewarner_diff",  "pd":   "pagewarner_diff",
+    
+    "?":    "help",             "h":    "help",
 
     // :::: work
 
@@ -148,14 +139,14 @@ const verbs = {
         if (!page) Err("fetch: Page can't be null.")
 
         const srcName = setting.sourceActive, src = setting.sources[srcName]
-        const _html = await ajax(src.url.replace(/\{page\}/g, page))
+        const _html = await ajax(src.url.replace(/\$\{page\}/g, page))
 
         let _title = _html.match(/<title>(.*)<\/title>/)[1]
         if (_title.match(/^[3-5][01]\d+/))
             Err(`fetch: HTTP error code: ${_title}`)
         _title = _title.replace(/ - 新笔趣阁.*/, "").replace("-", " @ ")
         const _content = _html.match(/<div id="content">(.*)<\/div>/)[1]
-            .replace(/<br[ ][\/]>/g, "\n")
+            .replace(/<br[ ][/]>/g, "\n")
             .replace(/readx\(\);/g, "")
             .replace(/&amp;/g, "&")
             .replace(/&nbsp;/g, " ")
@@ -167,32 +158,32 @@ const verbs = {
             _arround = _html.match(/=keypage;([\s\S]*)function keypage/)[1],
             _prev = _arround.match(/prevpage="\/(.*).html"/)?.[1],
             _next = _arround.match(/nextpage="\/(.*).html"/)?.[1],
-            arroundJSON =
-`{
-    "prev": ${_prev ? '"' + _prev + '"' : "null"},
-    "curr": "${page}",
-    "next": ${_next ? '"' + _next + '"' : "null"},
-    "title": "${_title}"
-}\n`
-        Log(arroundJSON)
-        await writeConfig("arround", arroundJSON)
+            arround = {
+                prev: _prev ?? null,
+                curr: page,
+                next: _next ?? null
+            }
+        Log(JSON.stringify(arround, null, 4) + "\n")
+        
+        arround.title = _title
+        await writeConfig("arround", arround)
 
-        const pagewarning = await readConfig("pagewarning")
+        const pagewarner = await readConfig("pagewarner")
         const today = Date.fromTimeZone(+8).fommat("yyyymmdd")
         // WwW: I don't think anyone can read these pages one day.
-        const Wnum = setting.warningPageNum ?? 0xffff
+        const n = setting.pagewarner.warnNum ?? 0xffff
 
-        if (pagewarning.date !== today) {
-            pagewarning.date = today
-            pagewarning.num = 0
+        if (pagewarner.date !== today) {
+            pagewarner.date = today
+            pagewarner.num = 0
         }
-        else pagewarning.num++
-        if (pagewarning.num >= Wnum) {
-            div("page number warning", 0, 2)
-            Warn(`You have reached the warning page number ${Wnum}.\nPage now: ${pagewarning.num}.\nSuggestion: stop.\n`)
+        else pagewarner.num++
+        if (pagewarner.num >= n) {
+            div("pagewarner", 0, 2)
+            Warn(`You have reached the warning page number ${n}.\nPage now: ${pagewarner.num}.\nSuggestion: stop.\n`)
         }
 
-        await writeConfig("pagewarning", pagewarning)
+        await writeConfig("pagewarner", pagewarner)
 
         div("EOF", 0, 1)
     },
@@ -203,24 +194,24 @@ const verbs = {
         div("EOF", 1, 1)
     },
 
-    prev: fetchAlias("prev"),
-    curr: fetchAlias("curr"),
-    next: fetchAlias("next"),
+    fetch_prev: fetchAlias("prev"),
+    fetch_curr: fetchAlias("curr"),
+    fetch_next: fetchAlias("next"),
 
     arround: async() => {
         div("arround", 0, 2)
-        Log(await readConfig("arround", true))
+        Log(await readConfig("arround", true) + "\n")
         div("EOF", 0, 1)
     },
 
-    bookcase: async() => {
+    book_show: async() => {
         div("bookcase", 0, 2)
         // TODO: better display
         Log(await readConfig("books", true))
         div("EOF", 1, 1) 
     },
 
-    bookmark: async() => {
+    book_mark: async() => {
         const arround = await readConfig("arround")
         const key = arround.curr.match(/(.*)\//)[1]
         const books = await readConfig("books")
@@ -235,7 +226,7 @@ const verbs = {
         div("EOF", 0, 1)
     },
 
-    bookfetch: async() => {
+    book_fetch: async() => {
         const name = args[1]
         if (!name) Err("bookfetch: book name can't be null.")
 
@@ -258,56 +249,89 @@ const verbs = {
         }
         else {
             const W = !! args[2]
-
             div(W ? "config write" : "config read", 0, 2)
 
             let path = args[1]
             if (path[0] !== "." && path[0] !== "[") path = "." + path
 
             const rsit = path.matchAll(/\.([_a-zA-Z][0-9_a-zA-Z]*)|\[(\d+)]/g)
-            let rs, c = setting, pa, f = true, t, key
+            let rs, c = setting, pa, f = true, keyT, key, modify
+
             while (! (rs = rsit.next()).done) {
-                t = !! rs.value[1], key = t ? rs.value[1] : Number(rs.value[2])
-                if (c && typeof c === "object") {
-                    if ((t && ! Array.isArray(c))
-                    || (!t && Array.isArray(c))) {
-                        pa = c
-                        c = pa[key]
-                    }
+                keyT = !! rs.value[1]
+                key = keyT ? rs.value[1] : Number(rs.value[2])
+                if (Is.objR(c)) {
+                    if ((keyT && ! Array.isArray(c))
+                    || (!keyT && Array.isArray(c)))
+                        pa = c, c = pa[key]
                     else {
                         if (W) Err("config: Path type conflicted.")
                         Log(undefined)
                         f = false; break
                     }
                 }
-                else if (c === undefined) W && (c = pa[key] = (t ? {} : []))
+                else if (c === undefined) W && (pa[key] = (keyT ? {} : []), c = pa[key])
                 else W && Err("config: Path type conflicted.")
             }
             if (W) {
-                pa[key] = [ "undefined", "x" ].includes(args[2])
+                pa[key] = [ "undefined", "/" ].includes(args[2])
                     ? undefined
-                    : JSON.parse(args[2][0] === "=" ? '"' + args[2].slice(1) + '"' : args[2])
+                    : JSON.parse(args[2] === "=" ? '"' + args[3] + '"' : args[2])
                 await writeConfig("setting", setting)
+
+                modify = pa[key], pa[key] = "${config_modify}"
             }
-            if (f) Log(JSON.stringify(W ? setting : c, null, 4))
+            if (f) Log(JSON.stringify(W ? setting : c, null, 4)
+                .replace(/"\$\{config_modify\}"/, Hili(JSON.stringify(modify)))
+            )
             div("EOF", 1, 1)
         }
+    },
+
+    config_edit: async() => {
+        const path = process.env.XBQG_DATA + "/" + (args[1] ?? "setting") + ".json"
+        const editor = setting.editor.replace(/\$\{path\}/g, path)
+
+        div("config edit", 0, 2)
+        Log("Running " + Hili("$ " + editor))
+        
+        await execa.command(editor, {
+            stdin: process.stdin, stdout: process.stdout, stderr: process.stderr
+        })
+        
+        Log(`Done.`)
+        div("EOF", 1, 1)
+    },
+
+    config_reset: async() => {
+        div("config reset", 0, 2)
+        Warn("The default setting will be restored in 5 sec.")
+        await sleep(5000)
+
+        Log("Reseting.")
+        await writeConfig("setting", configDft.setting)
+        Log("Done.")
+        div("EOF", 1, 1)
     }
 }
 
-async function init() {
-    const configDft = {
-        setting:        {
-            warningPageNum: 0xffff,
-            sourceActive: "xbqg",
-            sources: {
-                xbqg: { url: "https://www.xsbiquge.com/{page}.html", charset: "utf8" }
-            }
+const configDft = {
+    setting:        {
+        editor: "vi ${path}",
+        pagewarner: {
+            warnNum: 0xffff
         },
-        arround:        {},
-        books:          {},
-        pagewarning:    {}
-    }
+        sourceActive: "xbqg",
+        sources: {
+            xbqg: { url: "https://www.xsbiquge.com/${page}.html", charset: "utf8" }
+        }
+    },
+    arround:        {},
+    books:          {},
+    pagewarner:     {}
+}
+
+async function init() {
     for (let i in configDft)
         if (! await existFile(`${p_data}/${i}.json`))
             writeConfig(i, configDft[i])
